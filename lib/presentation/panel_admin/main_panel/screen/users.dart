35ea21449be4d7eb8dace5fa824da086bf428533
Wanako <84190429+Wanako0609@users.noreply.github.com>
2022-05-07 13:25:03 +0200

    patch 3

--- lib/presentation/panel_admin/main_panel/screen/users.dart
+++ lib/presentation/panel_admin/main_panel/screen/users.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import '../viewmodels/vm_users/vm_users.dart';
+import '../viewmodels/vm_users.dart';
 
 class UsersAdmin extends StatelessWidget {
   const UsersAdmin({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ExpenseList();
+    return const ExpenseListUser();
   }
 }
